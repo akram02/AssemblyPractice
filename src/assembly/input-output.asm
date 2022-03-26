babb33fe9f60f96e9461aeb1ff19e358e76d193b
Akram Khan <akrammiru@gmail.com>
2022-03-26 13:54:30 +0600

    added comment to input-output.asm

--- src/assembly/input-output.asm
+++ src/assembly/input-output.asm
@@ -1,11 +1,14 @@
-main proc
-    mov ah, 1
-    int 21h
-    mov bl, al
+;.model small means we have code segment and data segment
+;.stack 100h indicates memory size
+;.code
+main proc ; proc = procedure
+    mov ah, 1   ; a function to input integer
+    int 21h     ; calling input integer function. input data goes to al
+    mov bl, al  ; insert input data to bl
 
-    mov ah, 2
-    mov dl, bl
-    int 21h
+    mov ah, 2   ; display function
+    mov dl, bl  ; output data should goes to dl. here we insert data to dl
+    int 21h     ; finishing output / execute function
 
     exit:
     mov ah, 4ch
